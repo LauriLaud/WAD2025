38c1e1020febf0ac73f56530e14215daa7b02066
AlanErikK <alan.erik.kasuk@ut.ee>
2025-12-01 01:24:18 +0200

    Corrected styling

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,26 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import SignupView from "@/views/SignupView.vue";
-
+import iconImg from "@/assets/img/icon.png";
 
 const routes = [
 {
 path: "/",
 name: "home",
-component: HomeView
+component: HomeView,
+meta: {
+      title: "Home",
+      icon: iconImg
+    }
 },
 {
 path: "/signup",
 name: "signup",
-component: SignupView
+component: SignupView,
+meta: {
+      title: "Sign Up",
+      icon: iconImg
+    }
 }
 ];
 
@@ -21,6 +29,21 @@ const router = createRouter({
 history: createWebHashHistory(),
 routes
 });
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || "PostIt";
+
+  let favicon = document.querySelector("link[rel='icon']");
+  if (!favicon) {
+    favicon = document.createElement("link");
+    favicon.rel = "icon";
+    document.head.appendChild(favicon);
+  }
+  favicon.href = to.meta.icon;
+
+  next();
+});
+
 
 
 export default router;
